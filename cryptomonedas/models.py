5e1012c82412010259eb0d99a55391014173da1e
Jesus Martin <jesusmartinromero10@gmail.com>
2022-10-17 08:52:59 +0200

    Final

--- cryptomonedas/models.py
+++ cryptomonedas/models.py
@@ -1,8 +1,9 @@
-from lib2to3.pytree import convert
+from wtforms import HiddenField
 import sqlite3
 from config import *
 import requests
 from datetime import *
+from cryptomonedas.routes import *
 
 
 def filas_to_diccionario(filas, columnas):
@@ -55,58 +56,13 @@ def recuperado():
     cur.execute("SELECT (CASE WHEN SUM(Cantidad_to) IS NULL THEN 0 ELSE SUM(Cantidad_to) end) as Cantidad_to FROM movements WHERE Moneda_to = 'EUR'")
     result = filas_to_diccionario(cur.fetchall(), cur.description)
     conn.close()
-    return result
-
-def valorCompra():
-    conn= sqlite3.connect(ORIGIN_DATA)
-    cur = conn.cursor()
-    cur.execute("SELECT (SUM(Cantidad_from) - SUM(Cantidad_to ) ) as valorCompra FROM movements WHERE Moneda_to = 'EUR'")
-    result = filas_to_diccionario(cur.fetchall(), cur.description)
-    conn.close()
-    return result
-
-def union():
-    resultadazo = []
-    conn= sqlite3.connect(ORIGIN_DATA)
-    cur = conn.cursor()
-    cur.execute("SELECT Cantidad_from FROM movements WHERE Moneda_from = 'EUR'")
-    result = filas_to_diccionario(cur.fetchall(), cur.description)
-    resultadazo.append(result)
-    cur.execute("SELECT SUM(Cantidad_to) as Cantidad_to FROM movements WHERE Moneda_to = 'EUR'")
-    resulto = filas_to_diccionario(cur.fetchall(), cur.description)
-    resultadazo.append(resulto)
-    cur.execute("SELECT (SUM (Cantidad_from) - SUM(Cantidad_to ) ) as valorCompra FROM movements WHERE Moneda_to = 'EUR'")
-    resulta = filas_to_diccionario(cur.fetchall(), cur.description)
-    resultadazo.append(resulta)
-    conn.close()
-    return resultadazo
-
-def valorActual():
-    conn= sqlite3.connect(ORIGIN_DATA)
-    cur = conn.cursor()
-    cur.execute("SELECT (SELECT SUM(CANTIDAD_FROM) FROM movements WHERE Moneda_from = 'EUR')  as  eur,(SELECT SUM(CANTIDAD_FROM) FROM movements WHERE Moneda_from = 'BTC') as btc,(SELECT SUM(CANTIDAD_FROM) FROM movements WHERE Moneda_from = 'ETH') as eth FROM movements")
-    result = filas_to_diccionario(cur.fetchall(), cur.description)
-    conn.close()
-    return result 
-
-def cartera(moneda):
-    consulta = f"SELECT ((SELECT SUM(Cantidad_to) FROM movements WHERE Moneda_to = '{moneda}') - (SELECT SUM(Cantidad_from) FROM movements WHERE Moneda_from = '{moneda}')) AS {moneda}"
-    
-    conn= sqlite3.connect(ORIGIN_DATA)
-    cur = conn.cursor()
-    cur.execute(consulta)
-    result = filas_to_diccionario(cur.fetchall(), cur.description)
-    conn.close()
     return result  
 
-def cartera2(moneda, valor):
-    #consulta = f"SELECT ((select (case when Moneda_to is null then 0 else sum(Cantidad_to) end) as holaa from movements where Moneda_to = '{moneda}') - (select (case when Moneda_from is null then {valor} else sum(Cantidad_from) end) as hola from movements where Moneda_from = '{moneda}')) as tt"
+def cartera(moneda):
     consulta = f"SELECT ((SELECT (case when (SUM(Cantidad_to)) is null then 0 else SUM(Cantidad_to) end) as tot FROM movements WHERE Moneda_to = '{moneda}') - (SELECT (case when (SUM(Cantidad_from)) is null then 0 else SUM(Cantidad_from) end) as ee FROM movements WHERE Moneda_from = '{moneda}')) AS {moneda}"
-
 
     conn= sqlite3.connect(ORIGIN_DATA)
     cur = conn.cursor()
-    #cur.execute("SELECT ((select (case when (SUM(Moneda_to)) is null then 0 else SUM(Cantidad_to) end) as holaa from movements where Moneda_to = 'BTC') - (select (case when Moneda_from is null then 1222 else sum(Cantidad_from) end) as hola from movements where Moneda_from = 'BTC')) as tt from movements")
     cur.execute(consulta)
     result = filas_to_diccionario(cur.fetchall(), cur.description)
     conn.close()
@@ -118,35 +74,14 @@ def traerTodasCartera(crypto):
     conn= sqlite3.connect(ORIGIN_DATA)
     cur = conn.cursor()
     for moneda in crypto:
-        #consulta = f"SELECT ((SELECT SUM(Cantidad_to) FROM movements WHERE Moneda_to = '{moneda}') - (SELECT SUM(Cantidad_from) FROM movements WHERE Moneda_from = '{moneda}')) AS {moneda}"
         consulta = f"SELECT ((SELECT (case when (SUM(Cantidad_to)) is null then 0 else SUM(Cantidad_to) end) as tot FROM movements WHERE Moneda_to = '{moneda}') - (SELECT (case when (SUM(Cantidad_from)) is null then 0 else SUM(Cantidad_from) end) as ee FROM movements WHERE Moneda_from = '{moneda}')) AS {moneda}"
         cur.execute(consulta)
         fila =cur.fetchall() 
         cryptosMonedas[moneda] = fila[0][0]
-    #result = filas_to_diccionario(cur.fetchall(), cur.description)
     conn.close()
      
     return cryptosMonedas
 
-def totalActivo():
-    total = 0
-    monederoActual = traerTodasCartera(cryptos)
-    for clave in monederoActual.keys():
-        url = requests.get(f"https://rest.coinapi.io/v1/exchangerate/{clave}/EUR?&apikey={apikey}")
-        resultado = url.json()
-        valor = resultado['rate']
-        if monederoActual[clave] != None:
-            total += valor * monederoActual[clave]
-            
-
-    return total
-
-def borrar():
-    conn= sqlite3.connect(ORIGIN_DATA)
-    cur = conn.cursor()
-    cur.execute("DELETE from movements where id = (SELECT max(id) from movements)")
-    conn.commit() 
-    conn.close()
 
 def totalActivo_una_consulta():    
     total = 0
@@ -160,6 +95,38 @@ def totalActivo_una_consulta():
                 total += 1/b['rate'] * monederoActual[a]
                 
     return total
+
+
+def validador():
+    error = []
+    registros = select_all()
+    valorCantidad = request.values.get("inputCantidad") 
+    valorMonedaFrom = request.values.get('moneda_from')
+    valorMonedaTo = request.values.get('moneda_to')
+    valorCantidad2 = HiddenField
+
+    if registros == [] and valorMonedaFrom != "EUR":
+        error2 = flash("La primera compra de Cryptomonedas tiene que ser compradas con Euros")
+        error.append(error2)
+        return error
+       
+    if valorMonedaFrom == valorMonedaTo:
+        error2 = flash("Las monedas no pueden ser las mismas")
+        error.append(error2)
+        return error
+
+    if valorCantidad2._value != valorCantidad:
+        error2 = flash("Tienes que calcular el valor en el boton de calculadora antes de comprar pillin")
+        error.append(error2)
+        return error
+
+    monedero = cartera(valorMonedaFrom)
+    if (valorMonedaFrom != 'EUR' and monedero[0][valorMonedaFrom] < float(valorCantidad)):
+        error2 = flash(f"No tienes saldo suficiente de {valorMonedaFrom}")
+        error.append(error2)
+        return error
+    
+    return error
 
         
 
--- cryptomonedas/routes.py
+++ cryptomonedas/routes.py
@@ -3,10 +3,9 @@ from flask import redirect, render_template, request, url_for, flash
 from cryptomonedas import app
 import sqlite3
 from cryptomonedas.forms import Moneda
-from config import apikey, cryptos
-from cryptomonedas.models import cartera2, select_all, insert, peticion_crypto, invertido, recuperado, totalActivo_una_consulta, traerTodasCartera, union, valorActual, valorCompra, cartera, totalActivo, borrar
-from datetime import datetime, date
-import requests
+from config import apikey
+from cryptomonedas.models import select_all, insert, peticion_crypto, invertido, recuperado, totalActivo_una_consulta, validador
+from datetime import datetime
 from wtforms import HiddenField
 
 
@@ -25,10 +24,7 @@ def index():
 @app.route("/purchase", methods=["GET", "POST"])
 def comprar():
     moneda = Moneda()
-    registros = select_all()
     valorCantidad = request.values.get("inputCantidad") 
-    valorMonedaFrom = request.values.get('moneda_from')
-    valorMonedaTo = request.values.get('moneda_to')
     valorCantidad2 = HiddenField
     if request.method == "GET":
         return render_template("/purchase.html", PageTitle = "Comprar", formulario = moneda, cabecera = 'purchase.html', cantidad = "input")
@@ -39,7 +35,7 @@ def comprar():
                 
                 try:
                     if moneda.inputCantidad.data == None:
-                        flash("Introduce en la casilla cantidad un dato numerico o si es decimal usa el . no la , para los decimales")
+                        flash("Introduce en la casilla de Cantidad un dato numerico. O si es decimal usa el . (Punto) no la ,(Coma) para los decimales")
                         return redirect(url_for("comprar"))
                     resultado = peticion_crypto(moneda.moneda_from.data, moneda.moneda_to.data, apikey)
                     total = resultado['rate'] * float(valorCantidad)
@@ -48,7 +44,6 @@ def comprar():
                     tasa = ("{:.8f}".format(tasa))
                     valorCantidad2._value = valorCantidad
                     
-
                     return render_template("/purchase.html", resultado = total, Tasa = tasa, formulario = moneda, cabecera = "purchase.html", cantidad = "texto", valorinput = valorCantidad )
                 except Exception as e:
                     print(e)
@@ -56,40 +51,17 @@ def comprar():
                     return redirect(url_for("index"))
         
       
-
             elif request.values.get("submitCompra"):
                 
                 try:
-                    
-                    if registros == [] and valorMonedaFrom != "EUR":
-                        flash("La primera compra de Cryptomonedas tiene que ser compradas con Euros")
-                        return redirect(url_for("purchase.html"))
-                    if valorMonedaFrom == valorMonedaTo:
-                        flash("Las monedas no pueden ser las mismas")
-                        return redirect(url_for('comprar'))
-                    if valorCantidad2._value != valorCantidad:
-                        flash("Tienes que calcular el valor en el boton de calculadora antes de comprar pillin")
-                        return redirect(url_for("comprar"))
-                    
-#preguntat ¡¡¡ none monedero
-                    monedero = cartera2(valorMonedaFrom, valorCantidad)
-                    if (valorMonedaFrom != 'EUR' and monedero[0][valorMonedaFrom] < float(valorCantidad)):
-                        flash(f"No tienes saldo suficiente de {valorMonedaFrom}")
-                        return redirect(url_for('index'))
-                    
-
+                    validat = validador()
+                    if validat != []:
+                        return redirect (url_for('comprar'))
+                   
                     if moneda.validate():
                         resultado = peticion_crypto(moneda.moneda_from.data, moneda.moneda_to.data, apikey)
                         total = resultado['rate'] * float(valorCantidad)
                         insert([datetime.now().date().isoformat(), str(datetime.now().time().isoformat())[:8], resultado["asset_id_base"], valorCantidad, resultado["asset_id_quote"], total])
-
-                        #mone = cartera(valorMonedaFrom)
-                        #if valorMonedaFrom != 'EUR' and (mone[0][valorMonedaFrom] == None or mone[0][valorMonedaFrom] < 0):
-                         #   borrar()
-                          #  flash("Saldo insuficiente")
-                           # return redirect(url_for('index'))
-
-
                         flash("Compra realizada correctamente")
                         return redirect(url_for('index'))
                 except sqlite3.Error as e:
@@ -121,11 +93,8 @@ def estado():
             inv = invertido()
             rec = recuperado()
             vComp = inv[0]['Cantidad_from'] - rec[0]['Cantidad_to']
-            #valor_mio = traerTodasCartera(cryptos)
-
             vActi = totalActivo_una_consulta()
-
-
+        
             return render_template("status.html", inv = inv, rec = rec, vComp = vComp , vAct = vActi, ganancia = vActi - vComp, cabecera = 'status.html')
         except Exception as e:
             print(e)
